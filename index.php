1014d5b846530c5d8dea7285668ef1738631b70f
Nate Wright <natew@notthisway.com>
2017-08-31 11:50:05 +0100

    pkp/pkp-lib#723 Remove incorrect copyright statement

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 /**
  * @file plugins/generic/citationStyleLanguage/index.php
  *
- * Copyright (c) 2015-2016 University of Pittsburgh
  * Copyright (c) 2014-2017 Simon Fraser University
  * Copyright (c) 2003-2017 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
