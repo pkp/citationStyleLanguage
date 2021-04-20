203f66d232e98378bdf0ba6ffc4d23fdb184e31b
Nate Wright <natew@notthisway.com>
2021-04-20 16:32:01 +0100

    pkp/pkp-lib#5678 Format all PHP files to PSR12

--- CitationStyleLanguageSettingsForm.inc.php
+++ CitationStyleLanguageSettingsForm.inc.php
@@ -15,97 +15,104 @@
 
 import('lib.pkp.classes.form.Form');
 
-class CitationStyleLanguageSettingsForm extends Form {
+class CitationStyleLanguageSettingsForm extends Form
+{
+    /** @var object $plugin */
+    public $plugin;
 
-	/** @var $plugin object */
-	public $plugin;
+    /**
+     * Constructor
+     *
+     * @param $plugin object
+     */
+    public function __construct($plugin)
+    {
+        parent::__construct($plugin->getTemplateResource('settings.tpl'));
+        $this->plugin = $plugin;
+        $this->addCheck(new FormValidatorPost($this));
+        $this->addCheck(new FormValidatorCSRF($this));
+    }
 
-	/**
-	 * Constructor
-	 * @param $plugin object
-	 */
-	public function __construct($plugin) {
-		parent::__construct($plugin->getTemplateResource('settings.tpl'));
-		$this->plugin = $plugin;
-		$this->addCheck(new FormValidatorPost($this));
-		$this->addCheck(new FormValidatorCSRF($this));
-	}
+    /**
+    * @copydoc Form::init
+    */
+    public function initData()
+    {
+        $request = Application::get()->getRequest();
+        $context = $request->getContext();
+        $contextId = $context ? $context->getId() : 0;
+        $this->setData('primaryCitationStyle', $this->plugin->getSetting($contextId, 'primaryCitationStyle'));
+        $this->setData('enabledCitationStyles', array_keys($this->plugin->getEnabledCitationStyles($contextId)));
+        $this->setData('enabledCitationDownloads', $this->plugin->getEnabledCitationDownloads($contextId));
+        $this->setData('publisherLocation', $this->plugin->getSetting($contextId, 'publisherLocation'));
+    }
 
-	/**
-	* @copydoc Form::init
-	*/
-	public function initData() {
-		$request = Application::get()->getRequest();
-		$context = $request->getContext();
-		$contextId = $context ? $context->getId() : 0;
-		$this->setData('primaryCitationStyle', $this->plugin->getSetting($contextId, 'primaryCitationStyle'));
-		$this->setData('enabledCitationStyles', array_keys($this->plugin->getEnabledCitationStyles($contextId)));
-		$this->setData('enabledCitationDownloads', $this->plugin->getEnabledCitationDownloads($contextId));
-		$this->setData('publisherLocation', $this->plugin->getSetting($contextId, 'publisherLocation'));
-	}
+    /**
+     * Assign form data to user-submitted data.
+     */
+    public function readInputData()
+    {
+        $this->readUserVars([
+            'primaryCitationStyle',
+            'enabledCitationStyles',
+            'enabledCitationDownloads',
+            'publisherLocation',
+        ]);
+    }
 
-	/**
-	 * Assign form data to user-submitted data.
-	 */
-	public function readInputData() {
-		$this->readUserVars(array(
-			'primaryCitationStyle',
-			'enabledCitationStyles',
-			'enabledCitationDownloads',
-			'publisherLocation',
-		));
-	}
+    /**
+     * @copydoc Form::fetch()
+     *
+     * @param null|mixed $template
+     */
+    public function fetch($request, $template = null, $display = false)
+    {
+        $context = $request->getContext();
+        $contextId = $context ? $context->getId() : 0;
 
-	/**
-	 * @copydoc Form::fetch()
-	 */
-	public function fetch($request, $template = null, $display = false) {
-		$context = $request->getContext();
-		$contextId = $context ? $context->getId() : 0;
+        $allStyles = [];
+        foreach ($this->plugin->getCitationStyles() as $style) {
+            $allStyles[$style['id']] = $style['title'];
+        }
 
-		$allStyles = [];
-		foreach ($this->plugin->getCitationStyles() as $style) {
-			$allStyles[$style['id']] = $style['title'];
-		}
+        $allDownloads = [];
+        foreach ($this->plugin->getCitationDownloads() as $style) {
+            $allDownloads[$style['id']] = $style['title'];
+        }
 
-		$allDownloads = [];
-		foreach ($this->plugin->getCitationDownloads() as $style) {
-			$allDownloads[$style['id']] = $style['title'];
-		}
+        $templateMgr = TemplateManager::getManager($request);
+        $templateMgr->assign([
+            'pluginName' => $this->plugin->getName(),
+            'allDownloads' => $allDownloads,
+            'allStyles' => $allStyles,
+            'primaryCitationStyle' => $this->getData('primaryCitationStyle'),
+            'enabledStyles' => $this->plugin->mapCitationIds($this->plugin->getEnabledCitationStyles($contextId)),
+            'enabledDownloads' => $this->plugin->mapCitationIds($this->plugin->getEnabledCitationDownloads($contextId)),
+        ]);
 
-		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->assign(array(
-			'pluginName' => $this->plugin->getName(),
-			'allDownloads' => $allDownloads,
-			'allStyles' => $allStyles,
-			'primaryCitationStyle' => $this->getData('primaryCitationStyle'),
-			'enabledStyles' => $this->plugin->mapCitationIds($this->plugin->getEnabledCitationStyles($contextId)),
-			'enabledDownloads' => $this->plugin->mapCitationIds($this->plugin->getEnabledCitationDownloads($contextId)),
-		));
+        return parent::fetch($request, $template, $display);
+    }
 
-		return parent::fetch($request, $template, $display);
-	}
+    /**
+     * @copydoc Form::execute()
+     */
+    public function execute(...$functionArgs)
+    {
+        $request = Application::get()->getRequest();
+        $context = $request->getContext();
+        $contextId = $context ? $context->getId() : 0;
+        $this->plugin->updateSetting($contextId, 'primaryCitationStyle', $this->getData('primaryCitationStyle'));
+        $enabledCitationStyles = $this->getData('enabledCitationStyles') ? $this->getData('enabledCitationStyles') : [];
+        $this->plugin->updateSetting($contextId, 'enabledCitationStyles', $enabledCitationStyles);
+        $enabledCitationDownloads = $this->getData('enabledCitationDownloads') ? $this->getData('enabledCitationDownloads') : [];
+        $this->plugin->updateSetting($contextId, 'enabledCitationDownloads', $enabledCitationDownloads);
+        $this->plugin->updateSetting($contextId, 'publisherLocation', $this->getData('publisherLocation'));
 
-	/**
-	 * @copydoc Form::execute()
-	 */
-	public function execute(...$functionArgs) {
-		$request = Application::get()->getRequest();
-		$context = $request->getContext();
-		$contextId = $context ? $context->getId() : 0;
-		$this->plugin->updateSetting($contextId, 'primaryCitationStyle', $this->getData('primaryCitationStyle'));
-		$enabledCitationStyles = $this->getData('enabledCitationStyles') ? $this->getData('enabledCitationStyles') : array();
-		$this->plugin->updateSetting($contextId, 'enabledCitationStyles', $enabledCitationStyles);
-		$enabledCitationDownloads = $this->getData('enabledCitationDownloads') ? $this->getData('enabledCitationDownloads') : array();
-		$this->plugin->updateSetting($contextId, 'enabledCitationDownloads', $enabledCitationDownloads);
-		$this->plugin->updateSetting($contextId, 'publisherLocation', $this->getData('publisherLocation'));
+        import('classes.notification.NotificationManager');
+        $notificationMgr = new NotificationManager();
+        $user = $request->getUser();
+        $notificationMgr->createTrivialNotification($user->getId(), NOTIFICATION_TYPE_SUCCESS, ['contents' => __('common.changesSaved')]);
 
-		import('classes.notification.NotificationManager');
-		$notificationMgr = new NotificationManager();
-		$user = $request->getUser();
-		$notificationMgr->createTrivialNotification($user->getId(), NOTIFICATION_TYPE_SUCCESS, array('contents' => __('common.changesSaved')));
-
-		return parent::execute(...$functionArgs);
-	}
+        return parent::execute(...$functionArgs);
+    }
 }
-
--- pages/CitationStyleLanguageHandler.inc.php
+++ pages/CitationStyleLanguageHandler.inc.php
@@ -15,133 +15,137 @@
 
 import('classes.handler.Handler');
 
-class CitationStyleLanguageHandler extends Handler {
-	/** @var Submission article being requested */
-	public $article = null;
+class CitationStyleLanguageHandler extends Handler
+{
+    /** @var Submission article being requested */
+    public $article = null;
 
-	/** @var Publication publication being requested */
-	public $publication = null;
+    /** @var Publication publication being requested */
+    public $publication = null;
 
-	/** @var Issue issue of the publication being requested */
-	public $issue = null;
+    /** @var Issue issue of the publication being requested */
+    public $issue = null;
 
-	/** @var array citation style being requested */
-	public $citationStyle = '';
+    /** @var array citation style being requested */
+    public $citationStyle = '';
 
-	/** @var bool Whether or not to return citation in JSON format */
-	public $returnJson = false;
+    /** @var bool Whether or not to return citation in JSON format */
+    public $returnJson = false;
 
-	/**
-	 * Get a citation style
-	 *
-	 * @param $args array
-	 * @param $request PKPRequest
-	 * @return null|JSONMessage
-	 */
-	public function get($args, $request) {
-		$this->_setupRequest($args, $request);
+    /**
+     * Get a citation style
+     *
+     * @param $args array
+     * @param $request PKPRequest
+     *
+     * @return null|JSONMessage
+     */
+    public function get($args, $request)
+    {
+        $this->_setupRequest($args, $request);
 
-		$plugin = PluginRegistry::getPlugin('generic', 'citationstylelanguageplugin');
-		$citation = $plugin->getCitation($request, $this->article, $this->citationStyle, $this->issue, $this->publication);
+        $plugin = PluginRegistry::getPlugin('generic', 'citationstylelanguageplugin');
+        $citation = $plugin->getCitation($request, $this->article, $this->citationStyle, $this->issue, $this->publication);
 
-		if ($citation === false ) {
-			if ($this->returnJson) {
-				return new JSONMessage(false);
-			}
-			exit;
-		}
+        if ($citation === false) {
+            if ($this->returnJson) {
+                return new JSONMessage(false);
+            }
+            exit;
+        }
 
-		if ($this->returnJson) {
-			return new JSONMessage(true, $citation);
-		}
+        if ($this->returnJson) {
+            return new JSONMessage(true, $citation);
+        }
 
-		echo $citation;
-		exit;
-	}
+        echo $citation;
+        exit;
+    }
 
-	/**
-	 * Download a citation in a downloadable format
-	 *
-	 * @param $args array
-	 * @param $request PKPRequest
-	 */
-	public function download($args, $request) {
-		$this->_setupRequest($args, $request);
+    /**
+     * Download a citation in a downloadable format
+     *
+     * @param $args array
+     * @param $request PKPRequest
+     */
+    public function download($args, $request)
+    {
+        $this->_setupRequest($args, $request);
 
-		$plugin = PluginRegistry::getPlugin('generic', 'citationstylelanguageplugin');
-		$plugin->downloadCitation($request, $this->article, $this->citationStyle, $this->issue, $this->publication);
-		exit;
-	}
+        $plugin = PluginRegistry::getPlugin('generic', 'citationstylelanguageplugin');
+        $plugin->downloadCitation($request, $this->article, $this->citationStyle, $this->issue, $this->publication);
+        exit;
+    }
 
-	/**
-	 * Generate a citation based on page parameters
-	 *
-	 * @param $args array
-	 * @param $request PKPRequest
-	 */
-	public function _setupRequest($args, $request) {
-		$userVars = $request->getUserVars();
-		$user = $request->getUser();
-		$context = $request->getContext();
+    /**
+     * Generate a citation based on page parameters
+     *
+     * @param $args array
+     * @param $request PKPRequest
+     */
+    public function _setupRequest($args, $request)
+    {
+        $userVars = $request->getUserVars();
+        $user = $request->getUser();
+        $context = $request->getContext();
 
-		if (empty($userVars['submissionId']) || !$context || empty($args)) {
-			$request->getDispatcher()->handle404();
-		}
+        if (empty($userVars['submissionId']) || !$context || empty($args)) {
+            $request->getDispatcher()->handle404();
+        }
 
-		// Load plugin categories which might need to add data to the citation
-		PluginRegistry::loadCategory('pubIds', true);
-		PluginRegistry::loadCategory('metadata', true);
+        // Load plugin categories which might need to add data to the citation
+        PluginRegistry::loadCategory('pubIds', true);
+        PluginRegistry::loadCategory('metadata', true);
 
-		$this->citationStyle = $args[0];
-		$this->returnJson = isset($userVars['return']) && $userVars['return'] === 'json';
-		$this->article = Services::get('submission')->get($userVars['submissionId']);
+        $this->citationStyle = $args[0];
+        $this->returnJson = isset($userVars['return']) && $userVars['return'] === 'json';
+        $this->article = Services::get('submission')->get($userVars['submissionId']);
 
-		if (!$this->article) {
-			$request->getDispatcher()->handle404();
-		}
+        if (!$this->article) {
+            $request->getDispatcher()->handle404();
+        }
 
-		$this->publication = !empty($userVars['publicationId'])
-			? Services::get('publication')->get($userVars['publicationId'])
-			: $this->article->getCurrentPublication();
+        $this->publication = !empty($userVars['publicationId'])
+            ? Services::get('publication')->get($userVars['publicationId'])
+            : $this->article->getCurrentPublication();
 
-		if ($this->article) {
-			$issueDao = DAORegistry::getDAO('IssueDAO');
-			// Support OJS 3.1.x and 3.2
-			$issueId = method_exists($this->article, 'getCurrentPublication') ? $this->article->getCurrentPublication()->getData('issueId') : $this->article->getIssueId();
-			$this->issue = $issueDao->getById($issueId, $context->getId());
-		}
+        if ($this->article) {
+            $issueDao = DAORegistry::getDAO('IssueDAO');
+            // Support OJS 3.1.x and 3.2
+            $issueId = method_exists($this->article, 'getCurrentPublication') ? $this->article->getCurrentPublication()->getData('issueId') : $this->article->getIssueId();
+            $this->issue = $issueDao->getById($issueId, $context->getId());
+        }
 
-		// Disallow access to unpublished submissions, unless the user is a
-		// journal manager or an assigned subeditor or assistant. This ensures the
-		// article preview will work for those who can see it.
-		if (!$this->issue || !$this->issue->getPublished() || $this->article->getStatus() != STATUS_PUBLISHED) {
-			$userCanAccess = false;
+        // Disallow access to unpublished submissions, unless the user is a
+        // journal manager or an assigned subeditor or assistant. This ensures the
+        // article preview will work for those who can see it.
+        if (!$this->issue || !$this->issue->getPublished() || $this->article->getStatus() != STATUS_PUBLISHED) {
+            $userCanAccess = false;
 
-			if ($user && $user->hasRole([ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT], $context->getId())) {
-				$isAssigned = false;
-				$userGroupDao = DAORegistry::getDAO('UserGroupDAO');
-				$stageAssignmentDao = DAORegistry::getDAO('StageAssignmentDAO');
-				$assignments = $stageAssignmentDao->getBySubmissionAndStageId($this->article->getId());
-				foreach ($assignments as $assignment) {
-					if ($assignment->getUser()->getId() !== $user->getId()) {
-						continue;
-					}
-					$userGroup = $userGroupDao->getById($assignment->getUserGroupId($context->getId()));
-					if (in_array($userGroup->getRoleId(), [ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT])) {
-						$userCanAccess = true;
-						break;
-					}
-				}
-			}
+            if ($user && $user->hasRole([ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT], $context->getId())) {
+                $isAssigned = false;
+                $userGroupDao = DAORegistry::getDAO('UserGroupDAO');
+                $stageAssignmentDao = DAORegistry::getDAO('StageAssignmentDAO');
+                $assignments = $stageAssignmentDao->getBySubmissionAndStageId($this->article->getId());
+                foreach ($assignments as $assignment) {
+                    if ($assignment->getUser()->getId() !== $user->getId()) {
+                        continue;
+                    }
+                    $userGroup = $userGroupDao->getById($assignment->getUserGroupId($context->getId()));
+                    if (in_array($userGroup->getRoleId(), [ROLE_ID_SUB_EDITOR, ROLE_ID_ASSISTANT])) {
+                        $userCanAccess = true;
+                        break;
+                    }
+                }
+            }
 
-			if ($user && $user->hasRole(ROLE_ID_MANAGER, $context->getId())) {
-				$userCanAccess = true;
-			}
+            if ($user && $user->hasRole(ROLE_ID_MANAGER, $context->getId())) {
+                $userCanAccess = true;
+            }
 
-			if (!$userCanAccess) {
-				$request->getDispatcher()->handle404();
-			}
-		}
-	}
+            if (!$userCanAccess) {
+                $request->getDispatcher()->handle404();
+            }
+        }
+    }
 }
-
